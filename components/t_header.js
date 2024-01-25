52d218c22412665f0cc47a75ef6fbf7dd8510ec1
omar0ali <bajunaidomar@gmail.com>
2024-01-25 14:08:39 -0500

    Testing 1

--- components/t_header.js
+++ components/t_header.js
@@ -1,30 +1,37 @@
-import { getRealUrl } from './util.js';
+import { getRealUrl } from "./util.js";
 
 class HeaderComponent extends HTMLElement {
 	connectedCallback() {
-        this.loadContent();
-    }
-    async loadContent() {
-        let urlParts = []
-        let path = getRealUrl(window.location.href);
-        console.log("url: "+path)
-        this.urlParts = path.split('/');
-        if(path.includes(":")) {
-            this.path = ''
-        } else {
-            this.path = this.urlParts[this.urlParts.length-2]
-        }
-        console.log(this.urlParts)
-        console.log("uPath: "+this.path)
+		this.loadContent();
+	}
+	async loadContent() {
+		this.path = getRealUrl(window.location.href);
+		console.log("url: " + this.path);
+		this.urlParts = this.path.split("/");
+		if (this.path.includes(":")) {
+			this.path = "";
+		} else {
+			this.path = this.urlParts[this.urlParts.length - 2];
+		}
+		this.test = this.urlParts[this.urlParts.length - 2];
+		console.log(this.urlParts);
+		console.log(this.urlParts[this.urlParts.length - 2]);
+		console.log("uPath: " + this.path);
+        console.log("testPath: " + this.test)
 
-        this.content = this.innerHTML || ""
-        const header = await fetch(`${getRealUrl(window.location.href)}/components/header.html`);
-        const banner = await fetch(`${getRealUrl(window.location.href)}/components/topBanner.html`)
-        const txtHeader = await header.text();
-        const txtBanner = await banner.text();
-        let modifiedHeader = txtHeader.replace(/path/g, path)
-        this.innerHTML = txtBanner + modifiedHeader + `<h1>${this.content}</h1><hr>`;
-    }
+		this.content = this.innerHTML || "";
+		const header = await fetch(
+			`${getRealUrl(window.location.href)}/components/header.html`
+		);
+		const banner = await fetch(
+			`${getRealUrl(window.location.href)}/components/topBanner.html`
+		);
+		const txtHeader = await header.text();
+		const txtBanner = await banner.text();
+		let modifiedHeader = txtHeader.replace(/path/g, this.path);
+		this.innerHTML =
+			txtBanner + modifiedHeader + `<h1>${this.content}</h1><hr>`;
+	}
 }
 
 customElements.define("t-header", HeaderComponent);
