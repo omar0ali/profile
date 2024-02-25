010cc839c4d15e90e58bc0b297b580f5e3335671
omar0ali <bajunaidomar@gmail.com>
2024-02-25 04:32:35 -0500

    Updated contents and fixed typos

--- components/t_education.js
+++ components/t_education.js
@@ -1,12 +1,14 @@
 import { getRealUrl } from "./util.js";
 class EducationComponent extends HTMLElement {
-	connectedCallback() {
-        this.loadContent();
-    }
-    async loadContent() {
-        const html = await fetch(`${getRealUrl(window.location.href)}/components/education.html`);
-        this.innerHTML = await html.text();
-    }
+  connectedCallback() {
+    this.loadContent();
+  }
+  async loadContent() {
+    const html = await fetch(
+      `${getRealUrl(window.location.href)}/components/education.html`,
+    );
+    this.innerHTML = await html.text();
+  }
 }
 
 customElements.define("t-education", EducationComponent);
--- components/t_imports.js
+++ components/t_imports.js
@@ -1,19 +1,18 @@
 import { getRealUrl } from "./util.js";
 class ImportsComponent extends HTMLElement {
-	connectedCallback() {
-		const title = this.getAttribute("title") || "Title goes here";
-		this.innerHTML = `
+  connectedCallback() {
+    const title = this.getAttribute("title") || "Title goes here";
+    this.innerHTML = `
         <meta charset="utf-8">
-        <title>${this.title}</title>
+        <title>${this.title}</title> <!-- title is used -->
         <base href="/">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" type="image/x-icon" href="./favicon.ico">
-        <link href='https://fonts.googleapis.com/css?family=Fira Code' rel='stylesheet'>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link id="style" rel="stylesheet" href="${getRealUrl(window.location.href)}/style.css">
         `;
-	}
+  }
 }
 
 customElements.define("t-imports", ImportsComponent);
